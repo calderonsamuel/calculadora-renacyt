26e228b13f2adac5a6e629a9ac0b4696b7be25c1
calderonsamuel <samuel.calderon@uarm.pe>
2022-07-23 12:25:53 -0500

    dump case_when. ugly now

--- R/fun_puntajes.R
+++ R/fun_puntajes.R
@@ -9,19 +9,17 @@ get_calificacion <- function(puntaje_produccion = 0,
                              puntaje_formacion = 0, 
                              puntaje_total = 0, 
                              indice_h = "No") {
-    dplyr::case_when(
-        puntaje_produccion == 0 ~ "No califica: Requiere al menos un ítem en Producción",
-        puntaje_formacion == 1 & puntaje_produccion < 9 ~ "No califica: Estudiantes requieren 9 en producción",
-        puntaje_formacion > 1 & puntaje_produccion< 6  ~ "No califica: Requiere al menos 6 en producción",
-        puntaje_total < 10 ~ "No califica: Requiere al menos 10 en puntaje total",
-        puntaje_total <= 24 ~ "Sí califica: Nivel VII",
-        puntaje_total <= 34 ~ "Sí califica: Nivel VI",
-        puntaje_total <= 49 ~ "Sí califica: Nivel V",
-        puntaje_total <= 69 ~ "Sí califica: Nivel IV",
-        puntaje_total <= 99 ~ "Sí califica: Nivel III",
-        puntaje_total <= 159 ~ "Sí califica: Nivel II",
-        puntaje_total <= 199 ~ "Sí califica: Nivel I",
-        indice_h == "Sí" ~ "Investigador Distinguido",
-        TRUE ~ "Nivel I"
-    )
+    
+    if (puntaje_produccion == 0) return("No califica: Requiere al menos un ítem en Producción")
+    if (puntaje_formacion == 1 & puntaje_produccion < 9) return("No califica: Estudiantes requieren 9 en producción")
+    if (puntaje_formacion > 1 & puntaje_produccion < 6) return("No califica: Requiere al menos 6 en producción")
+    if (puntaje_total < 10) return("No califica: Requiere al menos 10 en puntaje total")
+    if (puntaje_total <= 24) return("Sí califica: Nivel VII")
+    if (puntaje_total <= 34) return("Sí califica: Nivel VI")
+    if (puntaje_total <= 49) return("Sí califica: Nivel V")
+    if (puntaje_total <= 69) return("Sí califica: Nivel IV")
+    if (puntaje_total <= 99) return("Sí califica: Nivel III")
+    if (puntaje_total <= 159) return("Sí califica: Nivel II")
+    if (puntaje_total <= 199) return("Sí califica: Nivel I")
+    if (indice_h == "Sí") return("Investigador Distinguido") else "Sí califica: Nivel I"
 }
